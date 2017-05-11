be3cd13f26e1a9468f90f7d901d187a5e86c92ea
ScuantynXD <Scuantyn@gmail.com>
2017-05-12 01:37:40 +0300

    modal

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var btn1 = document.querySelector(".modalbtn1");
 var btn2 = document.querySelector(".modalbtn2");
 var btn3 = document.querySelector(".modalbtn3");
-var prev1 = document.querySelector(".prevent1")
+/*var prev1 = document.querySelector(".prevent1")
 var prev2 = document.querySelector(".prevent2")
-var prev3 = document.querySelector(".prevent3")
+var prev3 = document.querySelector(".prevent3")*/
 var window1 = document.querySelector(".window1");
 var window2 = document.querySelector(".window2");
 var window3 = document.querySelector(".window3");
 console.log("Привет");
-prev1.addEventListener("click", function(event) {
+/*prev1.addEventListener("click", function(event) {
     event.preventDefault();
 });
 prev2.addEventListener("click", function(event) {
@@ -16,7 +16,7 @@ prev2.addEventListener("click", function(event) {
 });
 prev3.addEventListener("click", function(event) {
     event.preventDefault();
-});
+});*/
 btn1.addEventListener("click", function() {
     window1.classList.toggle("show");    
 });
