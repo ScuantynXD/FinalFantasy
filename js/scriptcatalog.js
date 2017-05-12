5aeb487925306cb3e71867d4bbfb309a5bd95e40
ScuantynXD <Scuantyn@gmail.com>
2017-05-12 16:22:39 +0300

    catalogJS

--- js/scriptcatalog.js
+++ js/scriptcatalog.js
@@ -3,6 +3,10 @@ var sorttext3 = document.querySelector(".sortingtext3");
 var sorttext4 = document.querySelector(".sortingtext4");
 var sortup = document.querySelector(".sortup");
 var sortdown = document.querySelector(".sortdown");
+var cataloglist1 = document.querySelector(".cataloglistbtn1");
+var cataloglist2 = document.querySelector(".cataloglistbtn2");
+var cataloglist3 = document.querySelector(".cataloglistbtn3");
+var cataloglist4 = document.querySelector(".cataloglistbtn4");
 console.log("Привет");
 sorttext2.addEventListener("click", function() {
     sorttext2.classList.toggle("active");
@@ -26,4 +30,28 @@ sortup.addEventListener("click", function() {
 sortdown.addEventListener("click", function() {
     sortdown.classList.toggle("active");
     sortup.classList.remove("active");        
+});
+cataloglist1.addEventListener("click", function() {
+    cataloglist1.classList.toggle("active");
+    cataloglist2.classList.remove("active");
+    cataloglist3.classList.remove("active");
+    cataloglist4.classList.remove("active");            
+});
+cataloglist2.addEventListener("click", function() {
+    cataloglist2.classList.toggle("active");
+    cataloglist1.classList.remove("active");
+    cataloglist3.classList.remove("active");
+    cataloglist4.classList.remove("active");            
+});
+cataloglist3.addEventListener("click", function() {
+    cataloglist3.classList.toggle("active");
+    cataloglist1.classList.remove("active");
+    cataloglist2.classList.remove("active");
+    cataloglist4.classList.remove("active");            
+});
+cataloglist4.addEventListener("click", function() {
+    cataloglist4.classList.toggle("active");
+    cataloglist1.classList.remove("active");
+    cataloglist2.classList.remove("active");
+    cataloglist3.classList.remove("active");            
 });
